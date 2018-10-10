4b416bb230a7ea993c055a4e370728efa01817e8
Luiz0tavio <me@luizotav.io>
2018-10-10 08:24:25 -0300

    Window visible - init buttons - Cells class

---
+++ src/Cell.java
@@ -0,0 +1,17 @@
+public class Cell
+{
+    public enum Type
+    {
+        NONE,
+        NOUGHT,
+        CROSS
+    }
+
+    private Type type = Type.NONE;
+
+    public Cell()
+    {
+
+    }
+
+}
--- src/Window.java
+++ src/Window.java
@@ -8,11 +8,21 @@ public class Window
     public Window()
     {
         frame = new JFrame();
+        frame.setSize(500, 500);
+        initButtons();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void setVisible(boolean b)
     {
         frame.setVisible(b);
+    }
+
+    private void initButtons()
+    {
+        Container container = frame.getContentPane();
+
+        JButton button = new JButton("Test");
+        container.add(button);
     }
 }
