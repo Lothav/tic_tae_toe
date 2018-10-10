51eccc1dcdf2de69ef9c778a9827d083844b93ae
Luiz0tavio <me@luizotav.io>
2018-10-10 08:50:37 -0300

    window size

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Window window = new Window();
+        Window window = new Window(500, 500);
         window.setVisible(true);
     }
 }
