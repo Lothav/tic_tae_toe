1169762364ccb42e130fcf5d9a287e2f16412946
Luiz0tavio <me@luizotav.io>
2018-10-10 08:38:12 -0300

    image assets - getImage() Cell method

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Cell
 {
     public enum Type
@@ -12,6 +14,27 @@ public class Cell
     public Cell()
     {
 
+    }
+
+    public ImageIcon getImage()
+    {
+        String path;
+
+        switch (type) {
+
+            case CROSS:
+                path = "assets/cross.png";
+                break;
+
+            case NOUGHT:
+                path = "assets/cross.png";
+                break;
+
+            default:
+                path = "assets/empty.png";
+        }
+
+        return new ImageIcon (path);
     }
 
 }
